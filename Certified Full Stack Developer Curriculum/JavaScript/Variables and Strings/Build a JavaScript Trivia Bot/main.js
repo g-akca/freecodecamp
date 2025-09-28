799a8c6e2c4f9447f317a60431890f4277ce7f47
g-akca <gok.akca66@gmail.com>
2025-09-29 01:56:07 +0300

    Variables and Strings - Build a JavaScript Trivia Bot

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Variables and Strings/Build a JavaScript Trivia Bot/main.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "MizuBot";
+let botLocation = "Tokyo";
+let favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Fun fact: " + favoriteLanguage + " was created in 10 days.";
+console.log(codingFact);
+
+codingFact = "Another fun fact: " + favoriteLanguage + " and Java are completely different languages.";
+console.log(codingFact);
+
+codingFact = "Finally, the last fun fact: " + favoriteLanguage + " is relied on by most web applications.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
