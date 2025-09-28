5007679e69712293673a2219e1b795e3ee574c2d
g-akca <gok.akca66@gmail.com>
2025-09-29 02:08:56 +0300

    Variables and Strings - Build a Sentence Maker

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Variables and Strings/Build a Sentence Maker/main.js
@@ -0,0 +1,19 @@
+let adjective = "beautfiul";
+let noun = "woman";
+let verb = "hot";
+let place = "villa";
+let adjective2 = "wide";
+let noun2 = "chicken";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "smart";
+noun = "programmer";
+verb = "cold";
+place = "small house";
+adjective2 = "normal";
+noun2 = "pizza";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
