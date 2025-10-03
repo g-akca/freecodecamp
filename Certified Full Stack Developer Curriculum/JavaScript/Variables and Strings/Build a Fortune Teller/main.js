0a6f3fe7ceee1adb3072526924d838ae7a69822c
g-akca <gok.akca66@gmail.com>
2025-10-03 19:26:15 +0300

    Variables and Strings - Build a Fortune Teller

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Variables and Strings/Build a Fortune Teller/main.js
@@ -0,0 +1,9 @@
+let fortune1 = "Your cat will look very cuddly today.";
+let fortune2 = "The weather will be nice tomorrow.";
+let fortune3 = "Be cautious of your new neighbors.";
+let fortune4 = "You will find a new hobby soon.";
+let fortune5 = "It would be wise to avoid the color red today.";
+
+let randomNumber = Math.floor(Math.random() * 5) + 1;
+let selectedFortune = randomNumber == 1 ? fortune1 : (randomNumber == 2 ? fortune2 : (randomNumber == 3 ? fortune3 : (randomNumber == 4 ? fortune4 : fortune5)));
+console.log(selectedFortune);
