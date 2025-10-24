71ac533b0706314e53a8e39c4a11b24eb93d84b7
g-akca <gok.akca66@gmail.com>
2025-10-24 18:59:17 +0300

    JavaScript Fundamentals Review - Build a Title Case Converter

---
+++ Certified Full Stack Developer Curriculum/JavaScript/JavaScript Fundamentals Review/Build a Title Case Converter/main.js
@@ -0,0 +1,8 @@
+function titleCase(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i == 0 || str[i - 1] == " ") result += str[i].toUpperCase();
+    else result += str[i].toLowerCase();
+  }
+  return result;
+}
