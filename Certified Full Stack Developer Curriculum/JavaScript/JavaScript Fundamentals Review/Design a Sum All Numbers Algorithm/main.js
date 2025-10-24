f1ffa97a6c8d37e66a6e90fde59eb7e2863102c8
g-akca <gok.akca66@gmail.com>
2025-10-24 19:14:04 +0300

    JavaScript Fundamentals Review - Design a Sum All Numbers Algorithm

---
+++ Certified Full Stack Developer Curriculum/JavaScript/JavaScript Fundamentals Review/Design a Sum All Numbers Algorithm/main.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
