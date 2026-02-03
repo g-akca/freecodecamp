e7ec9d29a8d35701faed17b46b2de8679400dbe8
g-akca <gok.akca66@gmail.com>
2026-02-04 01:47:20 +0300

    Add dynamic tabs to index.html, change topic structure

---
+++ script.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelectorAll('[role="tab"]');
+const panels = document.querySelectorAll('[role="tabpanel"]');
+const title = document.querySelector("#grid-container h3");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.setAttribute("aria-selected", "false"));
+    panels.forEach(p => p.hidden = true);
+
+    tab.setAttribute("aria-selected", "true");
+    const associatedPanel = tab.getAttribute("aria-controls");
+    const panel = document.getElementById(associatedPanel);
+    panel.hidden = false;
+    title.textContent = tab.textContent;
+  });
+});
