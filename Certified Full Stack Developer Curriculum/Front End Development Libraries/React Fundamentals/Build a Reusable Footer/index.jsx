967a156e180c5d34e79d8d06ad5f12dfe2dee2f8
g-akca <gok.akca66@gmail.com>
2026-02-15 01:17:04 +0300

    React Fundamentals - Build a Reusable Footer

---
+++ Certified Full Stack Developer Curriculum/Front End Development Libraries/React Fundamentals/Build a Reusable Footer/index.jsx
@@ -0,0 +1,22 @@
+export function Footer() {
+  return (
+    <footer>
+      <div>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Instagram</a></li>
+        </ul>
+        <ul>
+          <li><a href="#">Categories</a></li>
+          <li><a href="#">Store</a></li>
+        </ul>
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <p>© freeCodeCamp</p>
+    </footer>
+  );
+}
